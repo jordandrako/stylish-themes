ee487adf9af606f5c0110447a1727c3283b90c4a
Jordan Janzen <jordancjanzen@gmail.com>
2017-08-30 10:19:25 -0600

    ES6 gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,33 @@
 // Define gulp before we start
-var gulp = require("gulp");
+const gulp = require('gulp');
 // Define Sass and the autoprefixer
-var sass = require("gulp-sass");
-var prefix = require("gulp-autoprefixer");
+const sass = require('gulp-sass');
+const prefix = require('gulp-autoprefixer');
 // This is an object which defines paths for the styles.
 // Can add paths for javascript or images for example
 // The folder, files to look for and destination are all required for sass
-var paths = {
+const paths = {
   styles: {
-    src: "./src",
-    files: "./src/**/*.scss",
-    dest: "./dist"
-  }
+    src: './src',
+    files: './src/**/*.scss',
+    dest: './dist',
+  },
 };
 // A display error function, to format and make custom errors more uniform
 // Could be combined with gulp-util or npm colors for nicer output
-var displayError = function(error) {
+const displayError = (error) => {
   // Initial building up of the error
-  var errorString = "[" + error.plugin + "]";
-  errorString += " " + error.message.replace("\n", ""); // Removes new line at the end
+  let errorString = `[${error.plugin}]`;
+  errorString += ` ${error.message.replace('\n', '')}`; // Removes new line at the end
   // If the error contains the filename or line number add it to the string
-  if (error.fileName) errorString += " in " + error.fileName;
-  if (error.lineNumber) errorString += " on line " + error.lineNumber;
+  if (error.fileName) errorString += ` in ${error.fileName}`;
+  if (error.lineNumber) errorString += ` on line ${error.lineNumber}`;
   // This will output an error like the following:
   // [gulp-sass] error message in file_name on line 1
-  console.error(errorString);
+  console.error(errorString); // eslint-disable-line no-console
 };
 // Setting up the sass task
-gulp.task("sass", function() {
+gulp.task('sass', () => {
   // Taking the path from the above object
   gulp
     .src(paths.styles.files)
@@ -35,42 +35,30 @@ gulp.task("sass", function() {
     // Also pull the include path from the paths object
     .pipe(
       sass({
-        includePaths: [paths.styles.src]
-      })
+        includePaths: [paths.styles.src],
+      }),
     )
     // If there is an error, don't stop compiling but use the custom displayError function
-    .on("error", function(err) {
+    .on('error', (err) => {
       displayError(err);
     })
     // Pass the compiled sass through the prefixer with defined
-    .pipe(
-      prefix(
-        "last 2 version",
-        "safari 5",
-        "ie 8",
-        "ie 9",
-        "opera 12.1",
-        "ios 6",
-        "android 4"
-      )
-    )
+    .pipe(prefix('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     // Funally put the compiled sass into a css file
     .pipe(gulp.dest(paths.styles.dest));
 });
 // This is the default task - which is run when `gulp` is run
 // The tasks passed in as an array are run before the tasks within the function
-gulp.task("default", ["sass"], function() {
+gulp.task('default', ['sass'], () => {
   // Watch the files in the paths object, and when there is a change, fun the functions in the array
   gulp
-    .watch(paths.styles.files, ["sass"])
-    // Also when there is a change, display what file was changed, only showing the path after the 'sass folder'
-    .on("change", function(evt) {
+    .watch(paths.styles.files, ['sass'])
+    // Also when there is a change, display what file was changed,
+    // only showing the path after the 'sass folder'
+    .on('change', (evt) => {
+      // eslint-disable-next-line no-console
       console.log(
-        "[watcher] File " +
-          evt.path.replace(/.*(?=sass)/, "") +
-          " was " +
-          evt.type +
-          ", compiling..."
+        `[watcher] File ${evt.path.replace(/.*(?=sass)/, '')} was ${evt.type}, compiling...`,
       );
     });
 });
